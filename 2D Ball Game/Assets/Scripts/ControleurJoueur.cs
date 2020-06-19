4a389aa39ce7d690ac9ec5dbbef6506157bea7b8
J.Ezahraoui <ezahraouijad@gmail.com>
2020-06-19 14:30:49 +0100

    add new project

---
+++ 2D Ball Game/Assets/Scripts/ControleurJoueur.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ControleurJoueur : MonoBehaviour
+{
+
+    public float vitesse;
+    public Text countText;
+    public Text winText;
+    public Text loseText;
+    private Rigidbody rb;
+    private int count;
+    private int count1;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+        count1 = 0;
+        SetCountText();
+        winText.text = "";
+        loseText.text = "";
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        Vector3 mouvement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        rb.AddForce(mouvement * vitesse);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Cible"))
+        {
+            other.gameObject.SetActive(false);
+            count = count + 1;
+            SetCountText();
+        }
+        if (other.gameObject.CompareTag("Cube"))
+        {
+            other.gameObject.SetActive(false);
+            count1 = count1 + 1;
+            SetCountText();
+        }
+    }
+
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+        if (count >= 8)
+        {
+            loseText.text = "";
+            winText.text = "You Win!";
+            
+        }
+        if (count1 >= 1 && count<8)
+        {
+            loseText.text = "You Lose!";
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,20 +1,10 @@
 # Ellen's 2D Game
 
-<<<<<<< HEAD
 2D platformer made in Unity using 2D Game Kit. 
 
-
-
 ## Demo:
 
-![alt text](Ellen's 2D Game/Assets/Ellen's Game 2D.gif)
-=======
-2D platformer made in Unity using 2D Game Kit.
-
-## Demo:
-
-![image](https://drive.google.com/file/d/1wp_zaKz7cA51UunVpE8jCG3MU8rp51ph)
->>>>>>> b53c161a27fff67104a0370446b8d4ca9e9aee3f
+![image](https://github.com/Ezahraoui/Unity-Projects-cov19/blob/master/Ellen's%202D%20Game/Assets/Ellen's%20Game%202D.gif?raw=true)
 
 # Tools used:
 * Visual studio (IDE)
@@ -23,8 +13,4 @@
 
 # Contribution: 
 Feel free to `fork` this project and add whatever you like. If you have any suggestions or any comments please feel free to contact me or to open an issue. For contributions, just make a pull request describing the modification made.
-<<<<<<< HEAD
 
-
-=======
->>>>>>> b53c161a27fff67104a0370446b8d4ca9e9aee3f
