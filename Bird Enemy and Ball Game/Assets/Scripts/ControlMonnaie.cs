d7c0d659a6b51c54ebb1a608e22c6d835536edb1
J.Ezahraoui <ezahraouijad@gmail.com>
2020-06-20 09:37:20 +0100

    add Bird Enemy

---
+++ Bird Enemy and Ball Game/Assets/Scripts/ControlMonnaie.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class ControlMonnaie : MonoBehaviour
+{
+    public int score = 0;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Destroy(gameObject);
+        score = score + 1;
+        Debug.Log(score);
+    }
+}
---
+++ Bird Enemy and Ball Game/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float speed = 5f;
+    public float jumpSpeed = 5f;
+    private float movement = 0f;
+    private Rigidbody2D rigidBody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement = Input.GetAxis("Horizontal");
+        if (movement > 0f)
+        {
+            rigidBody.velocity = new Vector2(movement * speed, rigidBody.velocity.y);
+        }
+        else if (movement < 0f)
+        {
+            rigidBody.velocity = new Vector2(movement * speed, rigidBody.velocity.y);
+        }
+        else
+        {
+            rigidBody.velocity = new Vector2(0, rigidBody.velocity.y);
+        }
+        if (Input.GetButtonDown("Jump"))
+        {
+            rigidBody.velocity = new Vector2(rigidBody.velocity.x, jumpSpeed);
+        }
+        
+    }
+    void OnCollisionEnter(Collision exampleCol)
+    {
+        if (exampleCol.collider.tag == "Bird")
+        {
+            //Replace 'Game Over' with your game over scene's name.
+            SceneManager.LoadScene("Untitled");
+        }
+    }
+}
